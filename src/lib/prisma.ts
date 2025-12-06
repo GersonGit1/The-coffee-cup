4d48596427324bd1b5f503783802a58be56e10ed
GioDev <ga5715303@gmail.com>
2025-12-06 08:57:39 -0600

    add adapter for prisma-postgre

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,25 @@
 import { PrismaClient } from "@/src/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
 declare global {
-  // allow global prisma to survive module reloads during dev
   var prisma: PrismaClient | undefined;
 }
 
+const databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not set");
+}
+
 export const prisma =
-  global.prisma ||
+  globalThis.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query'] : []
+    adapter: new PrismaPg({
+      url: databaseUrl,
+    }),
+    log: process.env.NODE_ENV === "development" ? ["query"] : [],
   });
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
+
